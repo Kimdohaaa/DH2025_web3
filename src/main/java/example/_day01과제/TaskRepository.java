6116fe3d7842d9603e80a708f6652757ae5118ed
Kimdohaaa <jjeenee0408@gmail.com>
2025-04-01 15:15:22 +0900

    250401/ day01 과제

---
+++ src/main/java/example/_day01과제/TaskRepository.java
@@ -0,0 +1,8 @@
+package example._day01과제;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskEntity, String> {
+}
