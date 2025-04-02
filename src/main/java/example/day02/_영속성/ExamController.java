f610a450174fc8b4b23fcb063a112f11026ce4f7
Kimdohaaa <jjeenee0408@gmail.com>
2025-04-02 13:13:00 +0900

    250402 / JPA 영속성

---
+++ src/main/java/example/day02/_영속성/ExamController.java
@@ -0,0 +1,22 @@
+package example.day02._영속성;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/day02/persis")
+public class ExamController {
+
+    private final ExamService examService;
+
+    @GetMapping
+    public void get(){
+        System.out.println("ExamController.get");
+
+        examService.get();
+    }
+
+}
