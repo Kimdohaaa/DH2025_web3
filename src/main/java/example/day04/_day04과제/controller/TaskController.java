2565483ee180977090eeba2a06cd32dd99a29810
Kimdohaaa <jjeenee0408@gmail.com>
2025-04-10 17:14:12 +0900

    2504010 / CROS 허용

--- src/main/java/example/day04/_day04과제/controller/TaskController.java
+++ src/main/java/example/day04/_day04과제/controller/TaskController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/day04/task")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class TaskController {
 
     private final TaskService taskService;
